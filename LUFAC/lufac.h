748afe274d3f0e24c7ff42e837c85c26b63e77bc
Peter Foelsche <uhcu_63@outlook.com>
2024-10-09 15:57:04 -0500

    introduction of type for hessian matrix (3dim)

--- LUFAC/lufac.h
+++ LUFAC/lufac.h
@@ -10,6 +10,7 @@ namespace lufac
 {
 typedef std::map<std::size_t, double> index2Double;
 typedef std::map<std::size_t, index2Double> index2Index2Double;
+typedef std::map<std::size_t, index2Index2Double> index2Index2Index2Double;
 index2Index2Double factor(const index2Index2Double&_r, index2Double&);
 index2Double solve(const index2Index2Double&_r, const index2Double&_rY);
 }
