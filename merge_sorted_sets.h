40c33cb100352563c737297a17ba9c599275740e
Peter Foelsche <uhcu_63@outlook.com>
2024-12-11 16:56:54 -0600

    backup

--- merge_sorted_sets.h
+++ merge_sorted_sets.h
@@ -15,97 +15,81 @@ template<typename A>
 struct combineTwo<A, A>
 {	typedef A type;
 };
-template<template<typename, typename> class F, typename Set1, typename Set2, template<typename, typename> class MERGE=combineTwo>
-struct merge_sorted_sets;
-
-template<template<typename, typename> class F, template<typename, typename> class MERGE>
-struct merge_sorted_sets<F, mp_list<>, mp_list<>, MERGE>
-{	using type = mp_list<>;
-};
-
-template<template<typename, typename> class F, typename... Ts, template<typename, typename> class MERGE>
-struct merge_sorted_sets<F, mp_list<Ts...>, mp_list<>, MERGE>
-{	using type = mp_list<Ts...>;
-};
-
-template<template<typename, typename> class F, typename... Ts, template<typename, typename> class MERGE>
-struct merge_sorted_sets<F, mp_list<>, mp_list<Ts...>, MERGE>
-{	using type = mp_list<Ts...>;
-};
-
-template<template<typename, typename> class F, typename T1, typename... Ts1, typename T2, typename... Ts2, template<typename, typename> class MERGE>
-struct merge_sorted_sets<F, mp_list<T1, Ts1...>, mp_list<T2, Ts2...>, MERGE>
-{
-#if 1
+template<
+	template<typename, typename> class F,
+	typename SETS,
+	template<typename, typename> class MERGE=combineTwo
+>
+struct merge_sorted_set_of_sets
+{	template<typename T0, typename T1>
+	using compare=typename F<T0, T1>::type;
+	template<typename LIST>
+	using getMin = mp_front<
+		LIST
+	>;
+	typedef mp_remove_if<
+		SETS,
+		mp_empty
+	> SETS1;
+	typedef mp_transform<
+		getMin,
+		SETS1
+	> MINS;
+	typedef mp_min_element<
+		MINS,
+		compare
+	> MIN;
+	template<typename T>
+	using isMin = mp_and<
+		mp_not<compare<MIN, T> >,
+		mp_not<compare<T, MIN> >
+	>;
+	template<typename LIST>
+	using removeNotMin = mp_filter<isMin, LIST>;
+	typedef mp_transform<
+		removeNotMin,
+		SETS1
+	> SETS_NOT_MIN_REMOVED;
+	template<typename T>
+	using not_empty = mp_not<mp_empty<T> >;
+	typedef mp_filter<not_empty, SETS_NOT_MIN_REMOVED> SETS_NOT_MIN_REMOVED_3;
+	template<typename A, typename B>
+	using merge=typename MERGE<A, mp_front<B> >::type;
+	typedef mp_fold<
+		SETS_NOT_MIN_REMOVED_3,
+		MIN,
+		merge
+	> MINC;
+	template<typename T>
+	using isNotMin = mp_or<
+		compare<MIN, T>,
+		compare<T, MIN>
+	>;
+	template<typename LIST>
+	using removeMin = mp_filter<isNotMin, LIST>;
+	typedef mp_transform<
+		removeMin,
+		SETS1
+	> SETS_MIN_REMOVED;
+	typedef mp_remove_if<
+		SETS_MIN_REMOVED,
+		mp_empty
+	> SETS_MIN_REMOVED_2;
 	typedef typename std::conditional<
-		F<T1, T2>::type::value,
-		mp_list<
-			merge_sorted_sets<
-				F,
-				mp_list<Ts1...>,
-				mp_list<T2, Ts2...>,
-				MERGE
-			>,
-			mp_identity<T1>
-		>,
-		typename std::conditional<
-			F<T2, T1>::type::value,
-			mp_list<
-				merge_sorted_sets<
-					F,
-					mp_list<T1, Ts1...>,
-					mp_list<Ts2...>,
-					MERGE
-				>,
-				mp_identity<T2>
-			>,
-			mp_list<
-				merge_sorted_sets<
-					F,
-					mp_list<Ts1...>,
-					mp_list<Ts2...>,
-					MERGE
-				>,
-				MERGE<T2, T1>
-			>
-		>::type
-	>::type tmp;
-	typedef mp_push_front<
-		typename mp_first<tmp>::type,
-		typename mp_second<tmp>::type
-	> type;
-#else
-	typedef typename std::conditional<
-		F<T1, T2>::type::value,
-		mp_push_front<
-			typename merge_sorted_sets<
-				F,
-				mp_list<Ts1...>,
-				mp_list<T2, Ts2...>
-			>::type,
-			T1
-		>,
-		typename std::conditional<
-			F<T2, T1>::type::value,
-			mp_push_front<
-				typename merge_sorted_sets<
-					F,
-					mp_list<T1, Ts1...>,
-					mp_list<Ts2...>
-				>::type,
-				T2
-			>,
-			mp_push_front<
-				typename merge_sorted_sets<
-					F,
-					mp_list<Ts1...>,
-					mp_list<Ts2...>
-				>::type,
-				T2
-			>
-		>::type
-	>::type type;
-#endif
+		mp_empty<SETS_MIN_REMOVED_2>::value,
+		mp_identity<mp_list<> >,
+		merge_sorted_set_of_sets<F, SETS_MIN_REMOVED_2, MERGE>
+	>::type::type REST;
+	typedef mp_push_front<REST, MINC> type;
+};
+template<
+	template<typename, typename> class F,
+	typename Set1,
+	typename Set2,
+	template<typename, typename> class MERGE=combineTwo
+>
+struct merge_sorted_sets
+{	typedef typename merge_sorted_set_of_sets<F, mp_list<Set1, Set2>, MERGE>::type type;
 };
 }
 using implementation::merge_sorted_sets;
