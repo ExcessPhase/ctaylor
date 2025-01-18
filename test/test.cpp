10a1a84eaee7d80b1ebfb3cdcb39367419725377
Peter Foelsche <uhcu_63@outlook.com>
2025-01-18 16:37:09 -0600

    added testcase jacobian_4

--- test/test.cpp
+++ test/test.cpp
@@ -327,3 +327,11 @@ BOOST_AUTO_TEST_CASE(taylor_4)
 	const auto s5 = exp(-sqr(atan(1.0/s4 - s4*s4)));
 	__EQUAL_TAYLOR__();
 }
+BOOST_AUTO_TEST_CASE(jacobian_4)
+{	using namespace taylor;
+	using namespace boost::mp11;
+	const auto sMap = read("data4.txt", true);
+	const auto s4 = getS4Jacobian();
+	const auto s5 = exp(-sqr(atan(1.0/s4 - s4*s4)));
+	__EQUAL_JACOBIAN__();
+}
