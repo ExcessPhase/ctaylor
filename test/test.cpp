6de74d331cfd57b0ed00eea91782ae2eedb54f69
Peter Foelsche <uhcu_63@outlook.com>
2025-01-23 17:22:53 -0600

    added testcase for erfc()

--- test/test.cpp
+++ test/test.cpp
@@ -351,3 +351,19 @@ BOOST_AUTO_TEST_CASE(jacobian_5)
 	const auto s5 = erf(s4);
 	__EQUAL_JACOBIAN__();
 }
+BOOST_AUTO_TEST_CASE(taylor_6)
+{	using namespace taylor;
+	using namespace boost::mp11;
+	const auto sMap = read("data6.txt");
+	const auto s4 = getS4Taylor(false, mp_size_t<5>());
+	const auto s5 = erfc(s4);
+	__EQUAL_TAYLOR__();
+}
+BOOST_AUTO_TEST_CASE(jacobian_6)
+{	using namespace taylor;
+	using namespace boost::mp11;
+	const auto sMap = read("data6.txt", true);
+	const auto s4 = getS4Jacobian();
+	const auto s5 = erfc(s4);
+	__EQUAL_JACOBIAN__();
+}
