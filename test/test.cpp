ad6c7d1dccbc2b154bed8b1516a2dd0158c299f4
Peter Foelsche <uhcu_63@outlook.com>
2025-01-23 17:17:14 -0600

    added testcase fpr erf()

--- test/test.cpp
+++ test/test.cpp
@@ -335,3 +335,19 @@ BOOST_AUTO_TEST_CASE(jacobian_4)
 	const auto s5 = exp(-sqr(atan(1.0/s4 - s4*s4)));
 	__EQUAL_JACOBIAN__();
 }
+BOOST_AUTO_TEST_CASE(taylor_5)
+{	using namespace taylor;
+	using namespace boost::mp11;
+	const auto sMap = read("data5.txt");
+	const auto s4 = getS4Taylor(false, mp_size_t<5>());
+	const auto s5 = erf(s4);
+	__EQUAL_TAYLOR__();
+}
+BOOST_AUTO_TEST_CASE(jacobian_5)
+{	using namespace taylor;
+	using namespace boost::mp11;
+	const auto sMap = read("data5.txt", true);
+	const auto s4 = getS4Jacobian();
+	const auto s5 = erf(s4);
+	__EQUAL_JACOBIAN__();
+}
