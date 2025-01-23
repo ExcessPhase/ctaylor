19788d63e2652915504ff226cf003d63bcbbb849
Peter Foelsche <uhcu_63@outlook.com>
2025-01-23 17:30:36 -0600

    added testcase for sin()

--- test/test.cpp
+++ test/test.cpp
@@ -367,3 +367,19 @@ BOOST_AUTO_TEST_CASE(jacobian_6)
 	const auto s5 = erfc(s4);
 	__EQUAL_JACOBIAN__();
 }
+BOOST_AUTO_TEST_CASE(taylor_7)
+{	using namespace taylor;
+	using namespace boost::mp11;
+	const auto sMap = read("data7.txt");
+	const auto s4 = getS4Taylor(false, mp_size_t<5>());
+	const auto s5 = sin(s4);
+	__EQUAL_TAYLOR__();
+}
+BOOST_AUTO_TEST_CASE(jacobian_7)
+{	using namespace taylor;
+	using namespace boost::mp11;
+	const auto sMap = read("data7.txt", true);
+	const auto s4 = getS4Jacobian();
+	const auto s5 = sin(s4);
+	__EQUAL_JACOBIAN__();
+}
