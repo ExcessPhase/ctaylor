247cf238e93419b63df26e8f970d34a17d25fa58
Peter Foelsche <uhcu_63@outlook.com>
2025-01-23 17:36:15 -0600

    testcase for asin()

--- test/test.cpp
+++ test/test.cpp
@@ -383,3 +383,19 @@ BOOST_AUTO_TEST_CASE(jacobian_7)
 	const auto s5 = sin(s4);
 	__EQUAL_JACOBIAN__();
 }
+BOOST_AUTO_TEST_CASE(taylor_8)
+{	using namespace taylor;
+	using namespace boost::mp11;
+	const auto sMap = read("data8.txt");
+	const auto s4 = getS4Taylor(false, mp_size_t<5>());
+	const auto s5 = asin(1.0/s4);
+	__EQUAL_TAYLOR__();
+}
+BOOST_AUTO_TEST_CASE(jacobian_8)
+{	using namespace taylor;
+	using namespace boost::mp11;
+	const auto sMap = read("data8.txt", true);
+	const auto s4 = getS4Jacobian();
+	const auto s5 = asin(1.0/s4);
+	__EQUAL_JACOBIAN__();
+}
