68a614c76030ac211e14238ac4918ee2b68cec0c
Peter Foelsche <uhcu_63@outlook.com>
2025-01-24 11:49:28 -0600

    added testcase for cosh

---
+++ test/test17.cpp
@@ -0,0 +1,20 @@
+#include "test.h"
+#include <boost/test/unit_test.hpp>
+
+
+BOOST_AUTO_TEST_CASE(taylor_17)
+{	using namespace taylor;
+	using namespace boost::mp11;
+	const auto sMap = read("data17.txt");
+	const auto s4 = getS4Taylor(false);
+	const auto s5 = cosh(s4);
+	__EQUAL_TAYLOR__();
+}
+BOOST_AUTO_TEST_CASE(jacobian_17)
+{	using namespace taylor;
+	using namespace boost::mp11;
+	const auto sMap = read("data17.txt", true);
+	const auto s4 = getS4Jacobian();
+	const auto s5 = cosh(s4);
+	__EQUAL_JACOBIAN__();
+}
