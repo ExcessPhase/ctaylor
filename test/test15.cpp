9847ebcb5a6bffa478676eed0c67c3a1649e086f
Peter Foelsche <uhcu_63@outlook.com>
2025-01-24 11:41:26 -0600

    added tan() testcase

---
+++ test/test15.cpp
@@ -0,0 +1,20 @@
+#include "test.h"
+#include <boost/test/unit_test.hpp>
+
+
+BOOST_AUTO_TEST_CASE(taylor_15)
+{	using namespace taylor;
+	using namespace boost::mp11;
+	const auto sMap = read("data15.txt");
+	const auto s4 = getS4Taylor(false);
+	const auto s5 = tan(s4);
+	__EQUAL_TAYLOR__();
+}
+BOOST_AUTO_TEST_CASE(jacobian_15)
+{	using namespace taylor;
+	using namespace boost::mp11;
+	const auto sMap = read("data15.txt", true);
+	const auto s4 = getS4Jacobian();
+	const auto s5 = tan(s4);
+	__EQUAL_JACOBIAN__();
+}
